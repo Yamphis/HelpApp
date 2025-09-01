6aca637dcd0ed924f53a7c2c634af74e3c3d68df
ylopd <ylopezduprey@mail.valenciacollege.edu>
2025-08-31 22:54:25 -0400

    Initial Commit

---
+++ variable.py
@@ -0,0 +1,25 @@
+print('\n5 multiplied by 6')
+answerA=5*6
+print('The answer to your query is\n',answerA)
+
+print('\n8 divided by 3')
+answerB=8/3
+print('The answer to your query is\n',answerB)
+
+print('\n8 divided by 3 without the remainder')
+answerC=8/3
+print('The answer to your query is\n',int(answerC))
+
+print('\n8 modulo 3 (remainder only)')
+answerD=8%3
+print('The answer to your query is\n',answerD)
+
+print('\n4200000 added 23090')
+answerE=4200000+23090
+print('The answer to your query to your query is\n',answerE)
+
+print('\n4200000 subtract 7633')
+answerF=4200000-7633
+print('The answer to your query is\n',answerF)
+
+print('\nYamphis Lopez-Duprey')
